8c77d897b6b75f8a50b09d7eefd861312ab19088
Subhamay Paul <subhomaypaul@gmail.com>
2022-06-13 01:58:31 +0530

    uploading files

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,17 @@
-console.log('Express Tutorial')
+const express = require('express')
+const app = express()
+const logger = require('./logger')
+
+app.get('/', logger, (req,res)=>{
+    res.send('Home');
+})
+
+app.get('/about', logger, (req,res)=>{
+    res.send('About');
+})
+
+
+
+app.listen(8000, ()=>{
+    console.log('server is running on port 8000')
+})
